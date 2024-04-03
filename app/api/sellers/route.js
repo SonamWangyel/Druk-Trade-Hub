6a21f8f57dfde3bd12c033c5d5602db80f04e2e6
SonamWangyel <stevenrex229@gmail.com>
2024-04-03 12:03:21 +0600

    Modified some seller route

--- app/api/sellers/route.js
+++ app/api/sellers/route.js
@@ -3,7 +3,7 @@ import knex from '@/app/database'
 export async function GET(request) {
     const searchParams = request.nextUrl.searchParams
     const userId = searchParams.get('user_id')
-	const data = await (userId === null ? knex ('sellers').select('*') : knex('sellers').where('user_id', userId).select('* ') )
+	const data = await (userId === null ? knex ('sellers').join('users', 'sellers.user_id', 'users.id').select('sellers.*', 'users.name') : knex('sellers').join('users', 'sellers.user_id', 'users.id').where('user_id', userId).select('sellers.*', 'users.name') )
 	return Response.json({ data })
 }
 
